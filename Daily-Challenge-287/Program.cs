07af6e2999a81a3f154f62d525557ee9aac7f4c8
encid <ryancav@gmail.com>
2016-12-13 16:28:04 -0500

    Add project files.

---
+++ Daily-Challenge-287/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplication1 {
+    class Program {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("9184 -> " + LargestDigit(9184));
+            Console.ReadLine();
+        }
+
+        static string LargestDigit(int input)
+        {
+            string number = input.ToString();
+            switch (number.Length) {
+                case 1:
+                number = "000" + number;
+                break;
+                case 2:
+                number = "00" + number;
+                break;
+                case 3:
+                number = "0" + number;
+                break;
+                case 4:
+                break;
+                //default:
+                //return -1;
+            }
+
+            List<char> nums = number.OrderByDescending(x => x).ToList();
+
+            string s = "";
+            foreach (var item in nums) {
+                s += item;
+            }
+
+            return s;
+        }
+
+        static int DescendingDigits(int input)
+        {
+            return 1;
+        }
+    }
+}
