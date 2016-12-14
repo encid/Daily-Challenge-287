e7204916b3fdeac103f136f8576e9b4f8bd8168e
encid <ryancav@gmail.com>
2016-12-14 13:04:02 -0500

    finished bonuses

--- Daily-Challenge-287/Program.cs
+++ Daily-Challenge-287/Program.cs
@@ -8,32 +8,30 @@ namespace ConsoleApplication1 {
         static void Main(string[] args)
         {
             Console.WriteLine("Output number ordered from largest to smallest digit:");
-            Console.WriteLine("9184 -> " + DescendingDigits(9184));
-            Console.WriteLine("417 -> " + DescendingDigits(417));
-            Console.WriteLine("45 -> " + DescendingDigits(45));
-            Console.WriteLine("2850 -> " + DescendingDigits(2850));
-            Console.WriteLine("-----------------");
+            Console.WriteLine("9184 -> " + ConvertToDescending(9184));
+            Console.WriteLine("417 -> " + ConvertToDescending(417));
+            Console.WriteLine("45 -> " + ConvertToDescending(45));
+            Console.WriteLine("2850 -> " + ConvertToDescending(2850));
+
+            Console.WriteLine(" ");
             Console.WriteLine("Output the largest digit:");
             Console.WriteLine("9184 -> " + LargestDigit(9184));
             Console.WriteLine("417 -> " + LargestDigit(417));
             Console.WriteLine("45 -> " + LargestDigit(45));
             Console.WriteLine("2850 -> " + LargestDigit(2850));
 
+            Console.WriteLine(" ");
+            Console.WriteLine("Kaprekar routine iterations:");
+            Console.WriteLine("Kaprekar(6589) -> " + Kaprekar(6589));
+            Console.WriteLine("Kaprekar(5455) -> " + Kaprekar(5455));
+            Console.WriteLine("Kaprekar(6174) -> " + Kaprekar(6174));
+            Console.WriteLine("Kaprekar(1000) -> " + Kaprekar(1044));
+
+            Console.WriteLine(" ");
+            Console.WriteLine("Most Kaprekar iterations: " + LargestKaprekar().ToString());
 
             Console.ReadLine();
-        }
-
-        static string DescendingDigits(int input)
-        {
-            List<char> numList = ConvertNumberToDescendingList(input);
-
-            string s = "";
-            foreach (var item in numList) {
-                s += item;
-            }
-
-            return s;
-        }
+        }        
 
         static string LargestDigit(int input)
         {
@@ -49,6 +47,77 @@ namespace ConsoleApplication1 {
             List<char> numList = number.OrderByDescending(x => x).ToList();
 
             return numList;
+        }        
+
+        static int ConvertToDescending(int input)
+        {
+            string number = input.ToString("0000");
+            List<char> numList = number.OrderByDescending(x => x).ToList();
+
+            string s = "";
+            foreach (var item in numList) {
+                s += item;
+            }
+
+            int retVal = Convert.ToInt32(s);
+
+            return retVal;
         }
+
+        static int ConvertToAscending(int input)
+        {
+            string number = input.ToString("0000");
+            List<char> numList = number.OrderBy(x => x).ToList();
+
+            string s = "";
+            foreach (var item in numList) {
+                s += item;
+            }
+
+            int retVal = Convert.ToInt32(s);
+
+            return retVal;
+        }
+
+        static int Kaprekar(int input)
+        {
+            // Check if number is 4 digits
+            if (input < 1000 || input > 9999) 
+                return -1;            
+
+            // Check if number has at least 2 different digits
+            if (input.ToString().Distinct().Count() < 2)
+                return 0;
+            
+            int count = 0;
+            int result = input;           
+
+            while (result != 6174)
+            {
+                int a = ConvertToAscending(result);
+                int d = ConvertToDescending(result);
+                count += 1;
+                result = (d - a);
+            }
+
+            return count;
+        }
+
+        static int LargestKaprekar()
+        {
+            var kapDict = new Dictionary<int, int>();
+
+            for (int i = 1000; i < 10000; i++) {
+                kapDict.Add(i, Kaprekar(i));
+            }
+
+            var items = from pair in kapDict
+                        orderby pair.Value descending
+                        select pair;
+
+            var largestKap = items.ElementAt(0);
+
+            return largestKap.Value;
+        }        
     }
 }
