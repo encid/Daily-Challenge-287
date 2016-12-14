460b3609845007b565f23bfda6fa63fe3df9ba43
encid <ryancav@gmail.com>
2016-12-14 07:28:10 -0500

    added bonus

--- Daily-Challenge-287/Program.cs
+++ Daily-Challenge-287/Program.cs
@@ -7,45 +7,48 @@ namespace ConsoleApplication1 {
     class Program {
         static void Main(string[] args)
         {
+            Console.WriteLine("Output number ordered from largest to smallest digit:");
             Console.WriteLine("9184 -> " + DescendingDigits(9184));
             Console.WriteLine("417 -> " + DescendingDigits(417));
             Console.WriteLine("45 -> " + DescendingDigits(45));
             Console.WriteLine("2850 -> " + DescendingDigits(2850));
+            Console.WriteLine("-----------------");
+            Console.WriteLine("Output the largest digit:");
+            Console.WriteLine("9184 -> " + LargestDigit(9184));
+            Console.WriteLine("417 -> " + LargestDigit(417));
+            Console.WriteLine("45 -> " + LargestDigit(45));
+            Console.WriteLine("2850 -> " + LargestDigit(2850));
+
+
             Console.ReadLine();
         }
 
         static string DescendingDigits(int input)
         {
-            string number = input.ToString();
-            switch (number.Length) {
-                case 1:
-                number = "000" + number;
-                break;
-                case 2:
-                number = "00" + number;
-                break;
-                case 3:
-                number = "0" + number;
-                break;
-                case 4:
-                break;
-                //default:
-                //return -1;
-            }
-
-            List<char> nums = number.OrderByDescending(x => x).ToList();
+            List<char> numList = ConvertNumberToDescendingList(input);
 
             string s = "";
-            foreach (var item in nums) {
+            foreach (var item in numList) {
                 s += item;
             }
 
             return s;
         }
 
-        static int LargestDigit(int input)
+        static string LargestDigit(int input)
         {
-            return 1;
+            List<char> numList = ConvertNumberToDescendingList(input);
+
+            return numList[0].ToString();
+
+        }
+
+        static List<char> ConvertNumberToDescendingList(int input)
+        {
+            string number = input.ToString("0000");
+            List<char> numList = number.OrderByDescending(x => x).ToList();
+
+            return numList;
         }
     }
 }
